a24b5c5b886dfc07959acf1b944585e89389346e
adogins <124842665+adogins@users.noreply.github.com>
2024-03-15 10:50:21 -0400

    Added comments for instructions.

--- src/BSTTester.java
+++ src/BSTTester.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class BSTTester {
     public static void main (String[] args) {
         System.out.println("Test");
+        /* Uncomment generalTest if you want to enter numbers as input from the keyboard */
         generalTest();
         //runTests();
         } // main
